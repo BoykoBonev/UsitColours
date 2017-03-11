c0fb33cf519c582033fd440bb08096077718fae4
BoykoBonev <boykobonev@outlook.com>
2017-03-11 23:16:38 +0200

    aded test for test

---
+++ UsitColours.Tests/Services/AirlineService/Constructor_Should.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace UsitColours.Tests.Services.AirlineService
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void TestMethod()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
